e5a7eee97a7851c2f2bc6aa5f345168de6167eee
ammshahin <60013707+ammshahin@users.noreply.github.com>
2021-07-14 19:14:25 +0600

    core concepts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const products = [
+    { name: 'Photoshop', price: "500$" },
+    { name: 'Illustrator', price: "300$$" },
+    { name: 'Reader', price: "50$$" }
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Counter></Counter>
+        <User></User>
+        {
+          products.map(x => <li>{x.name}</li>)
+        }
+        <Person name="Shahin"></Person>
+        {
+          products.map(pd => <Products product = {pd}></Products>)
+        }
+
       </header>
     </div>
   );
+}
+function Products(props) {
+  const prdStyle = {
+    border: "1px solid grey",
+    borderRadius: "5px",
+    backgroundColor: "lightgrey",
+    height: "200px",
+    width: "200px",
+    float: "left"
+  }
+  return (
+    <div style={prdStyle}>
+      <h2>{props.product.name}</h2>
+      <h4>{props.product.price}</h4>
+      <button>Buy Now</button>
+    </div>
+  )
+}
+function Counter() {
+  const [count, setCount] = useState(5);
+  const increaseCount = () => setCount(count+1);
+  return(
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+    </div>
+  )
+}
+
+function User(){
+  const [user, setUser] = useState([]);
+  useEffect(() =>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => setUser(data));
+  },[])
+  return(
+    <div>
+      <h2>Dynamic Users: {user.length}</h2>
+      <ul>
+        {
+          user.map(user => <li>{user.name}</li>)
+        }
+        
+      </ul>
+    </div>
+  )
+}
+
+function Person(props) {
+  const pStyle = {
+    color: "green",
+    border: "4px solid yellow"
+  }
+  return (
+    <div style={pStyle} >
+      <h2>This is {props.name}</h2>
+      <h3>I am an Web freak</h3>
+    </div >
+
+  )
 }
 
 export default App;
